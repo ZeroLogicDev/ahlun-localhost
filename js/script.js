1747a15b3a77230d662121b446ca9f24a4a52237
ZeroLogicDev <ahlunnajarrudin@gmail.com>
2026-08-30 07:38:08 +0700

    feat: add spotify previews to terminal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const commandInput = document.getElementById('command-input');
-const outputArea = document.getElementById('output');
+const commandInput = document.getElementById("command-input");
+const outputArea = document.getElementById("output");
 
 function printOutput(message) {
     outputArea.innerHTML += `<div>${message}</div>`;
@@ -8,16 +8,23 @@ function printOutput(message) {
 
 window.onload = () => {
     printOutput("<br>Welcome to <strong>ahlun@localhost</strong>!");
-    printOutput("Type <span style='color: #e5c07b;'>'help'</span> to see the list of available commands.<br><br>");
-}
+    printOutput(
+        "Type <span style='color: #e5c07b;'>'help'</span> to see the list of available commands.<br><br>",
+    );
+};
 
-commandInput.addEventListener('keypress', function(e){
-    if(e.key === 'Enter'){
-        const command = commandInput.value.trim().toLowerCase();
-        printOutput(`<div><span style="font-weight: bold; color: #c9d1d9;"><span style="color: #3fb950;">ahlun@localhost</span>:<span style="color: #58a6ff;">~</span><span style="color: #c9d1d9;">$ ${command}</span></div>`);
+commandInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        const rawInput = commandInput.value.trim().toLowerCase();
+        const inputParts = rawInput.split(" ");
+        const command = inputParts[0];
+        const args = inputParts.slice(1);
+        printOutput(
+            `<div><span style="font-weight: bold; color: #c9d1d9;"><span style="color: #3fb950;">ahlun@localhost</span>:<span style="color: #58a6ff;">~</span><span style="font-weight: bold; color: #c9d1d9;">$ ${rawInput}</span></div>`,
+        );
 
         switch (command) {
-            case 'help':
+            case "help":
                 printOutput(`
                     Available commands: <br> 
                     <span style="color: #e5c07b;">motd</span> - Show this the massage of the day<br>
@@ -28,16 +35,17 @@ commandInput.addEventListener('keypress', function(e){
                     <span style="color: #e5c07b;">clear</span> - Clear the terminal output<br>
                 `);
                 break;
-            case 'whoami':
+            case "whoami":
                 printOutput(`
                     Hi, I'm <strong>Ahlun</strong>.<br><br>
                     I'm a full-stack web developer and a relentless tinkerer. I enjoy exploring new technologies, building innovative web applications, and sharing my knowledge with the community. My passion lies in creating seamless user experiences and efficient backend systems.<br><br>
                     When I'm not coding, you can find me experimenting with new programming languages, contributing to open-source projects, or indulging in my love for music and gaming. I believe in continuous learning and strive to stay updated with the latest trends in the tech world.<br><br>
-                    Feel free to connect with me on my social media platforms or check out my projects on GitHub. I'm always open to collaboration and new opportunities!
+                    Feel free to connect with me on my social media platforms or check out my projects on GitHub.
                     `);
                 break;
-            case 'music':
-                printOutput(`
+            case "music":
+                if (args.length === 0 || args[0] === "lists") {
+                    printOutput(`
                     <span style='color: #58a6ff; font-weight: bold;'>=== Favorite Music Tracks ===</span><br><br>
                     <span style='color: #3fb950;'>[+]</span> Memories<br>
                     <span style='color: #3fb950;'>[+]</span> Lose You To Love Me<br>
@@ -51,8 +59,70 @@ commandInput.addEventListener('keypress', function(e){
                     <span style='color: #3fb950;'>[+]</span> Self Healing<br><br>
                     <em>Note: These are just a few of my favorite tracks. I have a diverse taste in music and enjoy exploring different genres and artists.</em>
                     `);
+                } else if (args[0] === "play") {
+                    const trackNo = args[1];
+                    if (trackNo === "1") {
+                        printOutput("Loading track: Memories...<br>");
+                        printOutput(`
+                            <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5UXJzLFdBn6u9FJTCnoHrH?utm_source=generator&theme=0&si=3dc170a1a1624549" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                            `);
+                    } else if (trackNo === "2") {
+                        printOutput("Loading track: Lose You To Love Me...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/4l0Mvzj72xxOpRrp6h8nHi?utm_source=generator&theme=0&si=c61a1a7ccd07440d" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "3") {
+                        printOutput("Loading track: Rumors...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/4JMxLi40wVqq9To1QPSr4L?utm_source=generator&theme=0&si=c894513a5d704324" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "4") {
+                        printOutput("Loading track: Supercut...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/6K8VQ84MqhsoakN5MjrnVR?utm_source=generator&theme=0&si=ea268c28e41b4edd" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "5") {
+                        printOutput("Loading track: Somebody That I Used To Know...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/1qDrWA6lyx8cLECdZE7TV7?utm_source=generator&theme=0&si=b92c157b29a14b62" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "6") {
+                        printOutput("Loading track: I Won't Cry Anymore...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/0cQdt3fQ4yRvDmPdXhf51V?utm_source=generator&theme=0&si=af6f3fc31c0f4d2f" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "7") {
+                        printOutput("Loading track: Traitor...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5CZ40GBx1sQ9agT82CLQCT?utm_source=generator&theme=0&si=fe05ef8fe3584206" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "8") {
+                        printOutput("Loading track: Happier...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/2tGvwE8GcFKwNdAXMnlbfl?utm_source=generator&theme=0&si=509788ed9e554d6b" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "9") {
+                        printOutput("Loading track: Flowers...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/7DSAEUvxU8FajXtRloy8M0?utm_source=generator&theme=0&si=31bcbebf7b434e9a" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else if (trackNo === "10") {
+                        printOutput("Loading track: Self Healing...<br>");
+                        printOutput(`
+                        <iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/42ofgqIDT5yol8oPmG9UKc?utm_source=generator&theme=0&si=7c01e90b77b04868" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        `);
+                    } else {
+                        printOutput(
+                            `<span style="color: #f85149;">Invalid track number. Please use 'music play [track number]' to play a track.</span>`,
+                        );
+                    }
+                } else {
+                    printOutput(
+                        `<span style="color: #f85149;"Usage: music [lists|play [track number]]</span>`,
+                    );
+                }
                 break;
-            case 'network':
+            case "network":
                 printOutput(`
                     <span style='color: #58a6ff; font-weight: bold;'>=== Network & Social Links ===</span><br><br>
                     <span style='color: #3fb950;'>[+]</span> <strong>Github</strong>: <a href='https://github.com/ZeroLogicDev' target='_blank' style='color: #c9d1d9; text-decoration: underline;'>github.com</a><br>
@@ -62,7 +132,7 @@ commandInput.addEventListener('keypress', function(e){
                     <em>Click link above to open in a new tab.</em>
                     `);
                 break;
-            case 'neofetch':
+            case "neofetch":
                 printOutput(`
                     <div style="display: flex; gap: 35px; align-items: center; font-size: 1.16em;">
                     <pre style="color: #58a6ff; font-weight: bold; margin: 0; line-height: 1.2;">
@@ -111,9 +181,9 @@ commandInput.addEventListener('keypress', function(e){
                     </div>
                     `);
                 break;
-            case '':
+            case "":
                 break;
-            case 'motd':
+            case "motd":
                 printOutput(`
                     <pre style="color: #3fb950; font-weight: bold; margin: 0; line-height: 1.2;">
        _    _   _ _    _   _ _   _ 
@@ -133,20 +203,23 @@ commandInput.addEventListener('keypress', function(e){
                     Last updated: <span style='color: #58a6ff;'>27 aug 2026</span><br>
                     `);
                 break;
-            case 'clear':
-                outputArea.innerHTML = '';
+            case "clear":
+                outputArea.innerHTML = "";
                 break;
-            case 'date':
+            case "date":
                 const currentDate = new Date();
                 printOutput(`Current date and time: ${currentDate.toLocaleString()}`);
                 break;
-            case 'sudo':
-                printOutput(`<span style="color: #f85149;">You not allowed to use sudo command here. This incident will be reported.</span>`);
+            case "sudo":
+                printOutput(
+                    `<span style="color: #f85149;">You not allowed to use sudo command here. This incident will be reported.</span>`,
+                );
                 break;
             default:
-                printOutput(`<span style="color: #f85149;">${command}: command not found</span>`);
+                printOutput(
+                    `<span style="color: #f85149;">${command}: command not found</span>`,
+                );
         }
-        commandInput.value = '';
+        commandInput.value = "";
     }
 });
-
